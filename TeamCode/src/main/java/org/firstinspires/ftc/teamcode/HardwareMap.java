123686a3886f19298c8cce4b8ac48fb77d5c25f0
Flores <92351709+BrandonFloresY@users.noreply.github.com>
2022-10-27 18:36:22 -0500

    HardwareMap changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -1,4 +1,51 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+
 public class HardwareMap {
+    private LinearOpMode myOpMode = null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
